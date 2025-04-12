51c136ded2d37092439062ae57dc57623defc65b
panjatha <panjatha.mandeep@gmail.com>
2025-04-12 15:56:00 +0530

    added message for readme

--- child/storage_account/sa.tf
+++ child/storage_account/sa.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "sa" {
     name = "storagecicd123456"
-    resource_group_name = "rg-cicd"
+    resource_group_name = "rg-cicd2"
     location = "East US"
     account_tier = "Standard"
     account_replication_type = "LRS"
